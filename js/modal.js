443817597b08c4017270c0df16737dc4a46cb820
Gustavo Barretto <gustavobarretto@gmail.com>
2021-09-23 13:15:55 -0300

    adding input value out of modal

--- js/modal.js
+++ js/modal.js
@@ -2,10 +2,14 @@
 
 // Função para abrir o modal
 const modal = document.getElementById("modal");
-const abrirModal = document.querySelector("[class=addTarefa");
+const abrirModal = document.querySelector(".addTarefa");
+
 abrirModal.addEventListener("click", event => {
-    event.preventDefault();
-    modal.style.display = "block";
+    event.preventDefault()
+    let tituloForaModal = document.querySelector(".nova-tarefa input").value;
+    const tituloModal = document.querySelector("#modal form input");
+    tituloModal.value = tituloForaModal;
+    modal.style.display = "block";   
 })
 
 // Função para fechar o modal
@@ -17,6 +21,5 @@ fecharModal.addEventListener("click", () => {
 window.addEventListener("click", event => {
     if (event.target == modal) {
         modal.style.display = "none";
-    }
-    
+    }  
 })
