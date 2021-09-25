60849fca43ccee593de5d5848bad230ba00f1eb9
Gustavo Barretto <gustavobarretto@gmail.com>
2021-09-24 21:43:06 -0300

    resolving the double click in accordion and delete task

--- js/accordion.js
+++ js/accordion.js
@@ -1,34 +1,31 @@
 const accordion = document.querySelectorAll(".tasks");
-const x = window.scrollX+document
-          .querySelector(".far")
-          .getBoundingClientRect()
-          .top;
-const y = window.scrollY+document
-          .querySelector(".far")
-          .getBoundingClientRect()
-          .left;
-
-console.log(x)
-console.log(y)
 
 accordion.forEach(accordion => {
     accordion.addEventListener("click", () => {
         accordion.classList.toggle("active");
         let panel = accordion.lastElementChild;
-        if (panel.style.display == "block") {
-            panel.style.display = "none";
+        const trashBin = accordion.childNodes[5]
+        
+        console.log(trashBin.classList[2])
+
+        if(trashBin.classList[2] == "active") {
+            trashBin.classList.toggle("active")
+            accordion.classList.toggle("active")
         } else {
-            panel.style.display = "block";
-        }})})
+            if (panel.style.display == "block") {
+                panel.style.display = "none";
+            } else {
+                panel.style.display = "block";
+            }   
+        }
+    })})
 
-
-const tasks = document.querySelectorAll(".tasks");
+    const tasks = document.querySelectorAll(".tasks");
 
 tasks.forEach(tasks => {
     const trashBin = tasks.childNodes[5]
-    console.log(tasks.childNodes[5])
-    console.log(trashBin)
     trashBin.addEventListener("click", () => {
+        trashBin.classList.toggle("active")
         const deleteTask = confirm("Deseja realmente deletar?")
         deleteTask ? tasks.remove() : "";
     })})
