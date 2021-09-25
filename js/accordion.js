7057a031ed293aa48b6f0c245fbeff5b83ab9752
Gustavo Barretto <gustavobarretto@gmail.com>
2021-09-24 21:31:56 -0300

    trying to remove the accordion when cancel the delete

--- js/accordion.js
+++ js/accordion.js
@@ -1,4 +1,16 @@
-const accordion = document.querySelectorAll(".accordion");
+const accordion = document.querySelectorAll(".tasks");
+const x = window.scrollX+document
+          .querySelector(".far")
+          .getBoundingClientRect()
+          .top;
+const y = window.scrollY+document
+          .querySelector(".far")
+          .getBoundingClientRect()
+          .left;
+
+console.log(x)
+console.log(y)
+
 accordion.forEach(accordion => {
     accordion.addEventListener("click", () => {
         accordion.classList.toggle("active");
@@ -7,6 +19,16 @@ accordion.forEach(accordion => {
             panel.style.display = "none";
         } else {
             panel.style.display = "block";
-        }
-    })
-})
+        }})})
+
+
+const tasks = document.querySelectorAll(".tasks");
+
+tasks.forEach(tasks => {
+    const trashBin = tasks.childNodes[5]
+    console.log(tasks.childNodes[5])
+    console.log(trashBin)
+    trashBin.addEventListener("click", () => {
+        const deleteTask = confirm("Deseja realmente deletar?")
+        deleteTask ? tasks.remove() : "";
+    })})
